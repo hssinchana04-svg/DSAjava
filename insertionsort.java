10a2f114d8eccf2faefebebff0050ecd3e33e5ad
iamsinchana <hssinchana04@gmail.com>
2026-02-12 15:42:03 +0530

    add thisone

---
+++ insertionsort.java
@@ -0,0 +1,22 @@
+public class insertionsort {
+
+  public static void insertionsort(int arr[]){
+    for(int i=0; i<arr.length; i++){
+      int curr = arr[i];
+      int prev = i-1;
+      while(prev >= 0 && arr[prev] > curr){
+        arr[prev+1] = arr[prev];
+        prev--;
+      }
+      //insertion
+      arr[prev+1] = curr;
+
+    }
+  }
+  public static void main(String[] args) {
+    int arr[] = {5, 4, 1, 3, 2};
+    insertionsort(arr); 
+    for(int i=0; i<arr.length; i++){ 
+      System.out.print(arr[i] + " "); }
+  }
+}
