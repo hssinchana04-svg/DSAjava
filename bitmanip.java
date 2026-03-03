4e635521f8bd262550f8404f92af915ab4f50bfe
iamsinchana <hssinchana04@gmail.com>
2026-03-03 18:35:20 +0530

    bitmanipulation

---
+++ bitmanip.java
@@ -0,0 +1,102 @@
+import java.util.*;
+public class bitmanip {
+
+//   public static void oddOrEven(int n){
+//     int bitMask = 1;
+//     if((n & bitMask) == 0){
+//       System.out.println("Even");
+//   } else {
+//       System.out.println("Odd");
+//   }
+// }
+
+public static int getIthBit(int n, int i){
+  int bitmask = 1<<i;
+  if((n & bitmask) == 0){
+      return 0;
+    }else{
+      return 1;
+    }
+}
+
+public static int setIthBit(int n, int i){
+  int bitmask = 1<<i;
+  return n | bitmask;
+}
+
+public static int clearIthBit(int n, int i){
+  int bitmask = ~(1<<i);  
+  return n & bitmask;
+}
+
+public static int updateIthBit(int n, int i, int newBit){
+  // if(newBit == 0){
+  //   return clearIthBit(n, i);
+  // } else {
+  //   return setIthBit(n, i);
+  // }
+  n = clearIthBit(n, i);
+  int bitmask = newBit<<i;
+  return n | bitmask;
+}
+
+public static int clearIthBits(int n, int i){
+  int bitmask = (~0)<<i;  
+  return n & bitmask;
+}
+
+public static int clearRangeOfBits(int n, int i, int j){
+  int a = (~0)<<(j+1);
+  int b = (1<<i) - 1;
+  int bitmask = a | b;
+  return n & bitmask;
+}
+
+public static boolean isPowerOfTwo(int n){
+  return (n & (n-1)) == 0;
+}
+
+public static int countSetBits(int n){
+  int count = 0;
+  while(n > 0){
+    if((n & 1) != 0){
+      count++;
+    }
+    n = n>>1;
+
+  }
+  return count;
+}
+
+public static int fastExpo(int a, int n){
+  int ans = 1;
+
+  while(n>0){
+    if((n & 1) != 0){
+      ans = ans * a;
+    }
+    a = a*a;
+    n = n>>1;
+  }
+  return ans;
+}
+  public static void main(String[] args) {
+    // System.out.println(5&6);
+    // System.out.println(5|6);
+    // System.out.println(~6);
+    // System.out.println(5>>6);
+    // System.out.println(5<<6);
+    // oddOrEven(5);
+    //  oddOrEven(6);
+
+    System.out.println(getIthBit(10, 2));
+    System.out.println(setIthBit(10, 2));
+    System.out.println(clearIthBit(10, 1));
+    System.out.println(updateIthBit(10, 1, 1));
+    System.out.println(clearIthBit(15, 2));
+    System.out.println(clearRangeOfBits(31, 1, 3));
+    System.out.println(isPowerOfTwo(16));
+    System.out.println(countSetBits(15));
+    System.out.println(fastExpo(5, 2));
+  }
+}
